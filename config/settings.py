73217c54ccbde80e1ac1520880ff7311a1eeff20
Andrew Skow <skovorodnikov11@gmail.com>
2024-05-01 15:39:15 +0300

    installed apps

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # my apps
+    'habits',
+    'users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
