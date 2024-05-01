7691fcb350d00319a31fb8e3798ad4260e407fec
Andrew Skow <skovorodnikov11@gmail.com>
2024-05-01 15:54:43 +0300

    user_model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name='email'
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='is_active'
+    )
+    chat_id = models.CharField(
+        max_length=255,
+        verbose_name='number_of_chat',
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email} {self.is_active}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
