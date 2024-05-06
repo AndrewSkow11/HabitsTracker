db09cc6b5291a67bba60280b5f209230766af675
Andrew Skow <skovorodnikov11@gmail.com>
2024-05-06 18:29:55 +0300

    пагинация настроена

--- config/settings.py
+++ config/settings.py
@@ -107,6 +107,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
     "DEFAULT_PERMISSION_CLASSES": [
         'rest_framework.permissions.IsAuthenticated'],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
@@ -8,21 +9,43 @@ from habits.serializers import HabitSerializer
 
 from rest_framework.response import Response
 
+from rest_framework.pagination import PageNumberPagination
+
 
 class HabitAPIViewSet(ModelViewSet):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
     pagination_class = HabitPaginator
 
+
     def perform_create(self, serializer):
         new_habit = serializer.save()
         new_habit.user = self.request.user
         new_habit.save()
 
-    def list(self, serializer):
-        queryset = Habit.objects.filter(user=None)
-        serializer = HabitSerializer(queryset, many=True)
-        return Response(serializer.data)
+    # def list(self, request):
+    #     print("LIST")
+    #
+    #     paginator = PageNumberPagination()
+    #     paginator.page_size = 5
+    #
+    #
+    #     queryset = Habit.objects.filter(user=self.request.user)
+    #     serializer = HabitSerializer(queryset, many=True)
+    #     return Response(serializer.data)
+
+    def list(self, request):
+
+        print("ВЫЗОВ list")
+        page_size = 5
+
+        habit = Habit.objects.filter(user=self.request.user)
+        paginator = PageNumberPagination()
+        paginator.page_size = page_size
+        result_page = paginator.paginate_queryset(habit, request)
+        serializer = HabitSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
 
     def get_permissions(self):
         if self.action == 'create':
