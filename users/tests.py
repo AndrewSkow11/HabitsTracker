8204101e7a807481dfa841a2de07ce671d6a9884
Andrew Skow <skovorodnikov11@gmail.com>
2024-05-09 00:09:35 +0300

    test total 85%

--- users/tests.py
+++ users/tests.py
@@ -2,73 +2,60 @@ from rest_framework.test import APITestCase
 
 from rest_framework import status
 
-from habits.models import Habit
 from users.models import User
 
 
-class HabitTestCase(APITestCase):
+class UsersTestCase(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create(
-            email='smth@mail.com', is_superuser=True)
-
-        self.habit = Habit.objects.create(
-            place='your favorite place',
-            action='do_something')
+            email='smth@mail.com')
 
         self.user = User.objects.create(
             email='test@test.com',
-            is_superuser=True)
+            password='1234')
 
-    def test_get_list(self):
-        """TESTING READ (LIST)"""
-        self.client.force_authenticate(user=self.user)
-
-        response = self.client.get('/habits/')
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
-
-    def test_create_habit(self):
-        """TESTING CREATE"""
-        self.client.force_authenticate(user=self.user)
+    def test_create_user(self):
+        """TESTING CREATE User"""
 
         data = {
-            "place": self.habit.place,
-            "owner": self.user.id,
-            "action": self.habit.action
+            "email": "new_user@mail.com",
+            "password": "1234"
         }
 
-        response = self.client.post("/habits/", data)
+        response = self.client.post("/users/create/", data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        self.assertTrue(Habit.objects.all().exists())
+        self.assertTrue(User.objects.all().exists())
 
         self.assertEqual(
-            response.json()['place'],
-            data['place']
+            response.json()['email'],
+            data['email']
         )
 
-        self.assertEqual(
-            response.json()['action'],
-            data['action']
-        )
-
-    def test_update_habit(self):
-        """TESTING UPDATE"""
+    def test_get_list(self):
         self.client.force_authenticate(user=self.user)
-        habit_update = Habit.objects.create(
-            place='new habit ',
-            action='new random action',
-            user=self.user
+
+        response = self.client.get('/users/list/')
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+
+    def test_update_user(self):
+        """TESTING UPDATE User"""
+        self.client.force_authenticate(user=self.user)
+
+        user_update = User.objects.create(
+            email='smth2345@mail.com',
         )
 
         response = self.client.patch(
-            f'/habits/{habit_update.pk}/',
-            data={'place': 'new place'}
+            f'/users/{user_update.pk}/',
+            data={'email': 'something@smth.ru'}
         )
 
         self.assertEqual(
@@ -76,116 +63,5 @@ class HabitTestCase(APITestCase):
             status.HTTP_200_OK
         )
 
-        data = {'id': habit_update.id, 'place': 'new place', 'time_execute': '00:00:00',
-                'action': 'new random action',
-                'is_nice': False, 'periodicity': 1,
-                'reward': None, 'time_duration': 60,
-                'is_public': True, 'user': self.user.id,
-                'related_habit': None}
-
         self.assertEqual(
-            response.json(),
-            data
-        )
-
-    def test_delete_habit(self):
-        """ТESTING DELETE"""
-
-        self.client.force_authenticate(user=self.user)
-
-        habit_for_delete = Habit.objects.create(
-            place='deleting_place',
-            action='deleting_action',
-            user=self.user,
-        )
-
-        response = self.client.delete(
-            f'/habits/{habit_for_delete.id}/')
-
-        print(response)
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_204_NO_CONTENT
-        )
-
-    def test_retrieve_habit(self):
-        """TESTING UPDATE"""
-        self.client.force_authenticate(user=self.user)
-        habit_retrieve = Habit.objects.create(
-            place='new habit',
-            action='new random action',
-            user=self.user
-        )
-
-        response = self.client.patch(
-            f'/habits/{habit_retrieve.pk}/',
-            data={'is_nice': 'False'}
-        )
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
-
-        data = {'id': habit_retrieve.id, 'place': 'new habit',
-                'time_execute': '00:00:00',
-                'action': 'new random action', 'is_nice': False,
-                'periodicity': 1, 'reward': None, 'time_duration': 60,
-                'is_public': True, 'user': self.user.id, 'related_habit': None}
-
-        self.assertEqual(
-            response.json(),
-            data
-        )
-
-    def test_duration_habit(self):
-        """Тестирование создания привычки со временем исполнения более 2 минут"""
-
-        self.client.force_authenticate(user=self.user)
-
-        data = {
-            "time_duration": 130
-        }
-
-        response = self.client.post('/habits/', data=data)
-
-        self.assertEqual(
-            response.status_code, status.HTTP_400_BAD_REQUEST
-        )
-
-        self.assertEqual(
-            response.json(),
-            {'non_field_errors':
-                 ['Превышено время выполнение привычкине более 120 секунд)!']}
-        )
-
-    def test_periodicity_habit(self):
-        self.client.force_authenticate(user=self.user)
-
-        data = {
-            "periodicity": 8
-        }
-
-        response = self.client.post(
-            "/habits/",
-            data=data
-        )
-
-        self.assertEqual(
-            response.status_code, status.HTTP_400_BAD_REQUEST
-        )
-
-        self.assertEqual(
-            response.json(),
-            {'non_field_errors': ['Интервал задания периодичности [1; 7]!']}
-        )
-
-# andrejskovorodnikov@MacBook-Air-Andrej HabitsTracker % python3 manage.py test
-# Found 7 test(s).
-# Creating test database for alias 'default'...
-# System check identified no issues (0 silenced).
-# .<Response status_code=204>
-# ......
-# ----------------------------------------------------------------------
-# Ran 7 tests in 0.033s
+            response.json()['email'], 'something@smth.ru')
